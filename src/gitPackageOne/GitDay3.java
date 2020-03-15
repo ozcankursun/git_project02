3dc64ce3170af8b8cd155bdaca7f0695b45230ca
Abuzer Ozcan Kursun <ozcankursun@gmail.com>
2020-03-15 21:05:22 +0200

    Second Commit from local repo

--- src/gitPackageOne/GitDay3.java
+++ src/gitPackageOne/GitDay3.java
@@ -2,4 +2,26 @@ package gitPackageOne;
 
 public class GitDay3 {
 
+	public static void main(String[] args) {
+	
+		System.out.println("My first changes on local master");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
